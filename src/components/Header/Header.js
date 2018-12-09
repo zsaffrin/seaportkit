c661dced9e44d1fff95dea2a08efd719cb9b349a
Zach Saffrin <zach@zachsaffrin.com>
2018-12-08 23:17:32 -0500

    Update header layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => (
-    <header className="Header">
-        <h1>SeaportKit</h1>
+    <header>
+        <div className="primary">
+            <h1>SeaportKit</h1>
+        </div>
+        <div className="secondary">
+            <a
+                href="https://github.com/zsaffrin/seaportkit"
+                rel="noopener noreferrer"
+            >
+                GitHub
+            </a>
+        </div>
     </header>
 );
 
